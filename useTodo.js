41696aa9349a2fb03fa53e9ae69f4eedad2615bc
E-RI-CK <74332121+E-RI-CK@users.noreply.github.com>
2023-08-18 16:56:11 -0500

    First commit

---
+++ useTodo.js
@@ -0,0 +1,64 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const initialState = [
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del Alma',
+    //     done: false
+    // },
+    {
+        id: new Date().getTime() * 3,
+        description: 'Recolectar la piedra del Tiempo',
+        done: false
+    }
+];
+
+const init = () => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos'));
+    return storedTodos || initialState;
+}
+
+export const useTodo = () => {
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
+
+    const onNewTodo = (value) => {
+
+        const action = {
+            type: 'Add',
+            payload: value
+        }
+
+        dispatch(action);
+    }
+
+    const removeTodo = (id) => {
+        dispatch({
+            type: 'Remove',
+            payload: id
+        })
+    }
+
+
+    const onToggleTodo = (id) => {
+        dispatch({
+            type: 'Toggle',
+            payload: id
+        })
+    }
+
+    return {
+        todos,
+        onNewTodo,
+        removeTodo,
+        onToggleTodo,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(el => !el.done).length,
+    }
+}
